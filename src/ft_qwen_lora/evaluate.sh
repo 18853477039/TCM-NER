32be320d6105f3666deaecde8d673bff35b1d568
jishoukai <jishoukai@163.com>
2025-03-26 00:59:33 +0800

    feat: update evaluate

--- src/ft_qwen_lora/evaluate.sh
+++ src/ft_qwen_lora/evaluate.sh
@@ -3,7 +3,7 @@ lora_rank=8
 modules_to_save="null"
 lora_dropout=0.1
 LR=2e-4
-model_name_or_path="/root/autodl-fs/data2/root/.cache/modelscope/hub/models--THUDM--ChatGLM-6B/snapshots/bf0f5cfb575eebebf9b655c5861177acfee03f16"   # LLM底座模型路径，或者是huggingface hub上的模型名称
+model_name_or_path="/root/autodl-fs/data2/root/.cache/modelscope/hub/models--Qwen--Qwen2.5-7B-Instruct/snapshots/a09a35458c702b33eeacc393d103063234e8bc28"   # LLM底座模型路径，或者是huggingface hub上的模型名称
 your_data_path="./datasets/sft/"  # 填入数据集所在的文件夹路径
 your_data_path="./datasets/toys/"  # 填入数据集所在的文件夹路径
 
@@ -14,8 +14,8 @@ STEP=3500    # 用来评估的模型checkpoint是训练了多少步
 CUDA_VISIBLE_DEVICES=0 python src/ft_qwen_lora/main.py \
     --do_eval \
     --do_predict \
-    --validation_file $your_data_path/dev.json \
-    --test_file $your_data_path/test.json \
+    --validation_file $your_data_path/dev.jsonl \
+    --test_file $your_data_path/test.jsonl \
     --cache_dir $your_data_path \
     --overwrite_cache \
     --prompt_column input \
